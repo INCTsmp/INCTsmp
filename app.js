50c06153305dc5a51d1177ab75de5680c7490ccc
INCTsmp <inctsmp@gmail.com>
2025-04-11 15:50:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const netlifyEndpoint = 'https://twitchtracker.netlify.app/netlify/functions/get-status.js';
+const netlifyEndpoint = 'https://twitchtracker.netlify.app/netlify/functions/get-status';
 
 const trackedUsers = ['alwaysalmostnever', 'jawnskibop', 'radi0zombie'];
 
